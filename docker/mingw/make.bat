dce239dd8e10ef1ce7a3bf8f1631b7c835c867a1
linusyang92 <32575696+linusyang92@users.noreply.github.com>
2018-03-14 17:35:19 +0800

    Rename Docker image name

--- docker/mingw/make.bat
+++ docker/mingw/make.bat
@@ -2,7 +2,7 @@
 pushd %~dp0
 set "REPO=linusyang92"
 set "REV=mingw"
-set "IMAGE=ss-build-mingw"
+set "IMAGE=ss-build-mingw-asm"
 set "DIST=ss-libev-win-dist.tar.gz"
 docker build --force-rm -t %IMAGE% ^
       --build-arg REV=%REV% --build-arg REPO=%REPO% ^
